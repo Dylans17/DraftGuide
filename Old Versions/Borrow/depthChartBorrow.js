8238c75b8783ad94187b4fabe51948f64f9120f2
Dylan Strickley <dylans2499@gmail.com>
2022-07-31 13:43:27 -0400

    Removed versions folder

--- Old Versions/Borrow/depthChartBorrow.js
+++
@@ -1,8 +0,0 @@
-//https://www.fantasypros.com/nfl/depth-charts.php
-let arr = [];
-for (let i=0;i<128;i++) {
-  arr[i] = []
-  for (let j of document.body.getElementsByClassName("position-list")[i].getElementsByTagName("a")) {
-    arr[i].push(j.innerText)
-  }
-}
