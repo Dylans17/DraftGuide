d4cbaeed6bc75efcd00f1b672503dcf515daeeb1
Dylan Strickley <dylans2499@gmail.com>
2022-08-01 09:28:23 -0400

    Setup vite

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+    //my added options
+    outDir: "build",
+    rollupOptions: {
+      input: [ // list of input files
+        resolve(__dirname, "index.html")
+      ]
+    }
+  },
+  //root: resolve(__dirname, "src")
+});
