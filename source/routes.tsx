f6c3ba39d0ef929a1691e9f6f481251d6e92f34b
Dylan Strickley <dylans2499@gmail.com>
2022-08-02 18:28:39 -0400

    Less hardcoding for home. GetFileName generalized and moved to util

--- source/routes.tsx
+++ source/routes.tsx
@@ -1,30 +1,21 @@
 import { JSX, lazy } from "solid-js";
-import { Route, Routes } from "@solidjs/router";
+import { Navigate, Route, Routes } from "@solidjs/router";
+import { getFileName } from "./util";
 // glob import all components in ./component-pages
-const pages = import.meta.glob('./component-pages/*'); 
+const pages = import.meta.glob('./component-pages/*.jsx'); 
 
-type Import = () => Promise<{ default: () => JSX.Element;}>;
+type ImportComponent = () => Promise<{ default: () => JSX.Element;}>;
 
 export default () => {
-    let RouteFromComponentMap = () => {
-        return Object.entries(pages).map(([sourceFile, importFn], _) =>
-            <Route path={getPath(sourceFile)} component={lazy(importFn as Import)}></Route>
-        );
-    }
-    return <Routes> 
-      <RouteFromComponentMap />
-    </Routes>
-}
-
-function getPath(sourceFile: string):string {
-    if (sourceFile == "./component-pages/home.tsx") {
-        return "/";
-    }
-
-    let regex = /[a-z-]+(?=\.tsx$)/;
-    let result = regex.exec(sourceFile);
-    if (result === null) {
-        throw new Error(`Unable to detect file name from sourceFile: ${sourceFile}`);
-    }
-    return result[0];
+  let RouteFromComponentMap = () => {
+    return Object.entries(pages).map(([sourceFile, importFn], _) =>
+      <Route path={getFileName(sourceFile, "tsx")} component={lazy(importFn as ImportComponent)}></Route>
+    );
+  }
+  return <Routes> 
+    <RouteFromComponentMap />
+    <Route path="/">
+      <Navigate href="/home" />
+    </Route>
+  </Routes>
 }
