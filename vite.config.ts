97f7fb39828173f09721416912eb1482abd7af03
Dylan Strickley <dylans2499@gmail.com>
2022-08-04 18:00:06 -0400

    Fixed use of env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,55 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import { resolve } from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    VitePWA({
-      //https://vite-plugin-pwa.netlify.app/guide/pwa-minimal-requirements.html#web-app-manifest
-      includeAssets: ['source/assets/favicon.ico', 'source/assets/apple-touch-icon.png', 'source/assets/masked-icon.svg'],
-      manifest: {
-        name: 'Draft Guide',
-        short_name: 'Draft',
-        description: 'Draft Guide Web Application',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'source/assets/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'source/assets/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+export default ({ mode }: {mode: string}) => {
+  process.env = {...loadEnv(mode, process.cwd()), ...process.env};
+  return defineConfig({
+    plugins: [
+      solidPlugin(),
+      VitePWA({
+        //https://vite-plugin-pwa.netlify.app/guide/pwa-minimal-requirements.html#web-app-manifest
+        includeAssets: ['source/assets/favicon.ico', 'source/assets/apple-touch-icon.png', 'source/assets/masked-icon.svg'],
+        manifest: {
+          name: 'Draft Guide',
+          short_name: 'Draft',
+          description: 'Draft Guide Web Application',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'source/assets/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'source/assets/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+      //my added options
+      outDir: "build",
+      rollupOptions: {
+        input: [ // list of input files
+          resolve(__dirname, "index.html")
         ]
       }
-    })
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-    //my added options
-    outDir: "build",
-    rollupOptions: {
-      input: [ // list of input files
-        resolve(__dirname, "index.html")
-      ]
-    }
-  },
-  //root: resolve(__dirname, "src") //Project root directory (defaults to cwd)
-  base: import.meta.env.VITE_ROUTER_BASE || "/", //Base public path when served in development or production (Default: /)
-  css: {
-    modules: {
-      localsConvention: "camelCase"
     },
-  },
-});
+    //root: resolve(__dirname, "src") //Project root directory (defaults to cwd)
+    base: process.env.VITE_ROUTER_BASE || "/", //Base public path when served in development or production (Default: /)
+    css: {
+      modules: {
+        localsConvention: "camelCase"
+      },
+    },
+  });
+};
